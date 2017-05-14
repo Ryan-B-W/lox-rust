9bb73a67b7330305be2e91f748bdf7ab219fa954
David Nuon <davidnuongm@gmail.com>
2017-05-13 17:17:03 -0700

    Added conditional builds for macos and linux

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,30 @@ pub enum Shell {
     Fish,
     Bash
 }
+
+
+#[cfg(target_os = "macos")]
+pub fn get_parent_shell() -> String {
+    String::from("fish")
+}
+
+#[cfg(target_os = "linux")]
+pub fn get_parent_shell() -> String {
+    extern crate pentry;
+
+    let pid: i32;
+    unsafe {
+        pid = libc::getppid() as i32;
+    }
+
+    if let Ok(ps) = pentry::find(pid) {
+        let prog_option = ps.path().unwrap().split("/").collect::<Vec<&str>>();
+
+        match prog_option.last() {
+            Some(&v) => return v.to_owned(),
+            _ => panic!("Unable to get shell name"),
+        };
+    } else {
+        panic!("Unable to find shell PID")
+    }
+}
